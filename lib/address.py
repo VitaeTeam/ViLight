298824cf5ee78466ca417bf8b96301b548c24b78
Akshay <akshaycm@hotmail.com>
2019-11-20 00:03:57 +0300

    Use Legacy address type only

--- lib/address.py
+++ lib/address.py
@@ -483,7 +483,7 @@ class Address(namedtuple("AddressTuple", "hash160 kind")):
     _NUM_FMTS = 3  # <-- Be sure to update this if you add a format above!
 
     # Default to CashAddr
-    FMT_UI = FMT_CASHADDR
+    FMT_UI = FMT_LEGACY
 
     def __new__(cls, hash160, kind):
         assert kind in (cls.ADDR_P2PKH, cls.ADDR_P2SH)
